7cd5bfff604487c8092b2a6b82ba3cd10883ca95
CHENG-AN FU <fu908301@gmail.com>
2018-05-09 15:49:24 +0800

    Can connect to SQL now

--- enroll.php
+++ enroll.php
@@ -6,8 +6,24 @@
 </head>
 
 <body>
-<?php
-echo "這是註冊頁";
-?>
+	<form action="enroll2.php" name="enroll" method="post">
+	帳號:   
+	<input type="text" name="account">
+    <br>
+    <br>
+    密碼:   
+    <input type="password" name="pwd">
+    <br>
+    <br>
+    電話號碼:
+    <input type="text" name="phone">
+    <br>
+    <br>
+    Email:   
+    <input type="text" name="mail">
+	<br>
+    <br>
+    <input type ="submit" value="註冊">
+	</form>
 </body>
 </html>
---
+++ enroll2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+	<?php
+		$dbhost = "localhost";
+    	$dbuser = "root";
+    	$dbpass = "";
+    	$dbname = "auction";
+		$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname) or die('Error with MySQL connection');
+		
+		$account=$_REQUEST['account'];
+		$pwd=$_REQUEST['pwd'];
+		$phone=$_REQUEST['phone'];
+		$mail=$_REQUEST['mail'];
+		
+		mysqli_query($conn,"SET NAMES 'utf8'");
+		
+		$sql_insert="INSERT INTO user(nickname,pwd,phone,mail) VALUES('$account','$pwd','$phone','$mail')";
+		
+		$result = mysqli_query($conn,$sql_insert);
+		if(!$result){
+			echo "SQL insert error";
+			exit;
+		}
+		echo "註冊成功 即將回到首頁";	
+		sleep(3);
+		header("Location:http://127.0.0.1/lab/index.php");
+		
+	?>
+</body>
+</html>
