755b76c8268c54735ae15c07ae50870fcbb7e1d6
CHENG-AN FU <fu908301@gmail.com>
2018-05-20 14:58:04 +0800

    Buy page almost done.

--- buy.php
+++ buy.php
@@ -7,7 +7,31 @@
 
 <body>
 <?php
-echo "這是買家";
+		session_start();
+		$dbhost = "localhost";
+    	$dbuser = "root";
+    	$dbpass = "";
+    	$dbname = "auction";
+		$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname) or die('Error with MySQL connection');
+		mysqli_query($conn,"SET NAMES 'utf8'");
+		
+		$sql_show = "SELECT * FROM item";
+		$result = mysqli_query($conn,$sql_insert);
+		$row = mysqli_fetch_assoc($result);
+		if (mysqli_num_rows($result) > 0){
+				while($row = mysqli_fetch_assoc($result)){
+					$id = $row["sell_num"]; 
+					$sql_get_name = "SELECT nickname FROM user WHERE Num = '$id'";
+					$get_name = mysqli_query($conn,$sql_get_name);
+					echo "賣家姓名:" . $get_name["nickname"] . "<br>";
+					echo "商品名稱:" . $row["i_name"] . "<br>";
+					echo "商品分類:" . $row["i_class"] . "<br>";
+					echo "起標價:" . $row["s_price"] . "<br>";
+				}
+		}
+		else {
+			echo "目前沒有商品上架";	
+		}
 ?>
 </body>
 </html>
